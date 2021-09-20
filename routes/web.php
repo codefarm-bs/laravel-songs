34023dc04af2d16a3ccefd552d904100c5340458
Code Farm <codefarm.bs@gmail.com>
2021-09-20 10:53:14 +0430

    part2 - routes

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'CodeFarm';
+});
+
+Route::get('/contact', function () {
+    return 'Contact Us!!!';
 });
