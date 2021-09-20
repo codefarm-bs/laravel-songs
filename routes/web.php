c56202eebe32d28a3871bd6b494d1f33bec9eacc
Code Farm <codefarm.bs@gmail.com>
2021-09-20 12:24:19 +0430

    part 2 - views

--- routes/web.php
+++ routes/web.php
@@ -2,21 +2,5 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return 'CodeFarm';
-});
-
-Route::get('/contact', function () {
-    return 'Contact Us!!!';
-});
+Route::view('/','welcome');
+Route::view('contact','contact');
