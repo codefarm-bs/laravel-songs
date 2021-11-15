5ac17518738edca3e2c41edc050d430c64a8aa46
root <codefarm.bs@gmail.com>
2021-11-15 20:07:20 +0330

    part3 - send data to view

---
+++ resources/views/internal/users.blade.php
@@ -0,0 +1,8 @@
+<h1>Users</h1>
+
+<ul>
+    @foreach($users as $user)
+        <li>{{ $user }}</li>
+    @endforeach
+</ul>
+
--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/','welcome');
 Route::view('contact','contact');
+
+Route::get('users', function () {
+
+    $variable = [
+        'Ali',
+        'Hasan',
+        'Mohammad',
+        'fatemeh'
+    ];
+
+    return view('internal.users', [
+        'users' => $variable
+    ]);
+});
